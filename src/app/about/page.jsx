dc0b5597fcd078f5cbb20c2bd1ac2aac200b31e6
RushiReapmind <rushikesh.g@reapmind.com>
2024-08-07 15:02:04 +0530

    Coomit when changing the branch

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Page = () => {
-  return <div className="w-full">this is the about page which i changed</div>;
+  return <div className="w-full">this is the about page which i changed this is changed but not committed</div>;
 };
 
 export default Page;
