068f0c9b921a7277ce017f563f05dfd17e08b542
RushiReapmind <rushikesh.g@reapmind.com>
2024-08-07 15:09:47 +0530

    New content added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Page = () => {
-  return <div className="w-full">this is the about page which i changed this is changed but not committed</div>;
+  return (
+    <div className="w-full">
+      <div className="">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis vel earum
+        voluptas vero quam ea labore, quia magnam voluptatum sit fugit
+        distinctio asperiores ipsam commodi facere, dicta laudantium incidunt
+        reprehenderit!
+      </div>
+    </div>
+  );
 };
 
 export default Page;
